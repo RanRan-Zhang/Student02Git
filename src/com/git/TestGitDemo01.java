4c32506b99c16128059b84360bb79eb8470d6d6c
ranran01 <969634214@qq.com>
2021-05-14 18:02:40 +0800

    再次修改了

--- src/com/git/TestGitDemo01.java
+++ src/com/git/TestGitDemo01.java
@@ -5,4 +5,7 @@ public class TestGitDemo01 {
         System.out.println("这是TestGitDemo01");
         System.out.println("修改了TestGitDemo01");
     }
+    public  void add(){
+        System.out.println("新增方法");
+    }
 }
